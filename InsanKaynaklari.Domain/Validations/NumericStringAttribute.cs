b193a1871c598f098e0f852cfd61015c537931b9
Burak-Ozgur <122914501+Burak-Ozgur@users.noreply.github.com>
2023-07-31 20:12:46 +0300

    Add files via upload

---
+++ InsanKaynaklari.Domain/Validations/NumericStringAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsanKaynaklari.Domain.Validations
+{
+    public class NumericStringAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is string numericString && decimal.TryParse(numericString, out decimal result) && result >= 0)
+            {
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult(ErrorMessage ?? "ExpenseAmount must be a valid non-negative numeric value.");
+        }
+    }
+}
